d57802268bd33df7db16383a88e30620fd436ee0
juanluissimancasprundea25-create <juanluis.simancasprundea.25@campusfp.es>
2026-01-15 11:04:25 +0100

    Ejercicio3.php

--- Ejercicio3.php
+++ Ejercicio3.php
@@ -7,6 +7,7 @@ if ($_POST) {
     $fraseHackeada = str_ireplace($buscar, $reemplazar, $frase);
 }
 ?>
+<!-- Parte html -->
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -19,7 +20,7 @@ if ($_POST) {
         <textarea name="frase" rows="4" cols="50" placeholder="Escribe la frase que quieres hackear" required></textarea><br><br>
         <button type="submit">Hackear</button>
     </form>
-
+<!--Parte de php en la que realizamos la sustitucion de lo que escribimos -->
     <?php if ($fraseHackeada): ?>
         <h3>La frase hackeada es:</h3>
         <p><strong><?= htmlspecialchars($fraseHackeada) ?></strong></p>
